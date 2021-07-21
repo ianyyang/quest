ecec0dceae45ea0ac86a4affa1c3ff33fa716e53
Ian Yang <ianyyang00@gmail.com>
2021-07-21 01:19:50 -0400

    feat: primitive skeleton BookmarksSection

--- src/features/BookmarksSection/BookmarksSection.tsx
+++ src/features/BookmarksSection/BookmarksSection.tsx
@@ -1,9 +1,15 @@
+import Bookmark from 'common/Bookmark/Bookmark'
+import AddIcon from '@material-ui/icons/Add';
 import './BookmarksSection.css'
 
 const BookmarksSection = () => {
   return (
     <div className="bookmarks-section-container">
-      BookmarksSection
+      <Bookmark/>
+      <Bookmark/>
+      <Bookmark/>
+      <Bookmark/>
+      <AddIcon className="bookmarks-section-add-icon"/>
     </div>
   )
 }
