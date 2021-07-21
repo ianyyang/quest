b2c12dc73d34b497830eb210eb2ace884bbdc7a2
Ian Yang <ianyyang00@gmail.com>
2021-07-21 00:45:16 -0400

    feat: primitive skeleton OptionsMenu

--- src/features/OptionsMenu/OptionsMenu.tsx
+++ src/features/OptionsMenu/OptionsMenu.tsx
@@ -1,9 +1,12 @@
+import MinimizeIcon from '@material-ui/icons/Minimize'
 import './OptionsMenu.css'
 
 const OptionsMenu = () => {
   return (
     <div className="options-menu-container">
-      OptionsMenu
+      <MinimizeIcon className="options-menu-minimize-icon"/>
+      OPTIONS
+      <hr className="options-menu-hr"/>
     </div>
   )
 }
